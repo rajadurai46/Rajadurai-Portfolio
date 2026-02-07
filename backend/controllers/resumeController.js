f7e4b830b5cd3551069bdd6e7b81c20de205c0ad
raja46 <rj46jr@gmail.com>
2026-02-07 12:51:38 +0530

    second

--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -2,10 +2,11 @@ const path = require("path");
 
 /* Download Resume */
 const downloadResume = (req, res) => {
-  const filePath = path.join(__dirname, "..", "uploads", "resume.pdf");
+  const filePath = path.join(__dirname, "..", "public", "Resume.pdf");
 
-  res.download(filePath, "Resume.pdf", (err) => {
+  res.download(filePath, "Rajadurai-Resume.pdf", (err) => {
     if (err) {
+      console.log(err);
       res.status(500).json({ message: "Download failed" });
     }
   });
--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 /* Static folder (optional) */
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 /* Routes */
 app.use("/api/resume", require("./routes/resumeRoutes"));
