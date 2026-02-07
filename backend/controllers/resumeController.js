6c6b3ad719bb8533978d2ec602285d4f6c8ea64f
raja46 <rj46jr@gmail.com>
2026-02-07 12:27:38 +0530

    third

--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -4,7 +4,7 @@ const path = require("path");
 const downloadResume = (req, res) => {
   const filePath = path.join(__dirname, "..", "uploads", "resume.pdf");
 
-  res.download(filePath, "Rajadurai_A_Resume.pdf", (err) => {
+  res.download(filePath, "Resume.pdf", (err) => {
     if (err) {
       res.status(500).json({ message: "Download failed" });
     }
